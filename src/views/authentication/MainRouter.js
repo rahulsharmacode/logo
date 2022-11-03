878edf950099044a0ba53a48331a7afb0c3d8da9
rahulsharma <sharma56629@gmail.com>
2022-11-03 18:19:01 +0545

    Initial commit

---
+++ src/views/authentication/MainRouter.js
@@ -0,0 +1,21 @@
+const MainRoutes = {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+        {
+            path: '/',
+            element: <DashboardDefault />
+        },
+        {
+            path: 'dashboard',
+            children: [
+                {
+                    path: 'default',
+                    element: <DashboardDefault />
+                }
+            ]
+        }
+    ]
+};
+
+export default MainRoutes;
---
+++ src/views/pages/Homepage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../css/home.css'
+
+const Homepage = () => {
+  return (
+    <>
+    
+    <div className="card-wrapper">
+     <div className="card-body">
+     
+     <div className="line line-1"></div>
+      <div className="line line-2"></div>
+      <div className="line line-3"></div>
+      <div className="line line-4"></div>
+      <div className="para-box">
+        <h2>HTML<span>&</span>CSS</h2>
+        <small>design & build website</small>
+      </div>
+     
+     </div>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Homepage
